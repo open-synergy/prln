5da3d2e5020ef1e44dc64d199180027ef1150d1a
mike <mike@mike.(none)>
2015-11-16 21:58:00 +0700

    Creating a first structure module directory

---
+++ aeroo_opnsyn_pralon/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Michael Viriyananda
+#    Copyright 2015 Opensynergy Indonesia
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Pralon Report Module Based Using Aeroo Report ',
+    'version': '6.1.1.0.0',
+    'author': "Michael Viriyananda,Opensynergy Indonesia",
+    'category': 'Reporting',
+    'depends': ['account'],
+    'description': """
+    """,
+    'website': 'http://opensynergy-indonesia.com',
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
