a3a8618b395547e8f46719485c0613f831bba318
mike <mike@mike.(none)>
2015-11-18 14:07:17 +0700

    1. Templating report aging account payable

--- prln_aging_payable_report/report/report_aging_account_payable.py
+++ prln_aging_payable_report/report/report_aging_account_payable.py
@@ -29,4 +29,48 @@ class Parser(report_sxw.rml_parse):
         super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
             'time': time,
+            'get_invoice_date_from' : self.get_invoice_date_from,
+            'get_invoice_date_to' : self.get_invoice_date_to,
         })
+        
+    def convert_date(self, date):
+        convert_date = ''
+        nama_bulan = ''
+        
+        if date:
+
+            date_tahun = date[0:4]
+            date_bulan = date[5:7]
+            date_tanggal = date[8:10]
+                   
+            bulan = {
+                    '01' : 'Januari',
+                    '02' : 'Februari',
+                    '03' : 'Maret',
+                    '04' : 'April',
+                    '05' : 'Mei',
+                    '06' : 'Juni',
+                    '07' : 'Juli',
+                    '08' : 'Agustus',
+                    '09' : 'September',
+                    '10' : 'Oktober',
+                    '11' : 'November',
+                    '12' : 'Desember'
+                    }
+                    
+            nama_bulan = bulan.get(date_bulan, False)
+        
+            convert_date = date_tanggal + ' ' + nama_bulan + ' ' + date_tahun
+        
+        return convert_date
+        
+    def get_invoice_date_from(self):
+        invoice_date_from = self.localcontext['data']['form']['invoice_date_from']
+        convert_date_from = self.convert_date(invoice_date_from)
+        return convert_date_from
+        
+    def get_invoice_date_to(self):
+        invoice_date_to = self.localcontext['data']['form']['invoice_date_to']
+        convert_date_to = self.convert_date(invoice_date_to)
+        
+        return convert_date_to
