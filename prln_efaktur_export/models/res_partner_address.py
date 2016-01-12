b049c5c8b3d988671216ef3a4f2849511adcdc33
Andhitia Rama <andhitia.r@gmail.com>
2016-01-12 07:31:22 +0700

    Fix fields definition

--- prln_efaktur_export/models/res_partner_address.py
+++ prln_efaktur_export/models/res_partner_address.py
@@ -9,21 +9,27 @@ class res_partner_address(osv.osv):
     _columns = {
         'address_number': fields.char(
             string='Number',
+            size='10',
             ),
         'address_block': fields.char(
             string='Block',
+            size='10',
             ),
         'address_rt': fields.char(
             string='RT',
+            size='10',
             ),
         'address_rw': fields.char(
             string='RW',
+            size='10',
             ),
         'address_kec': fields.char(
             string='Kecamatan',
+            size='100',
             ),
         'address_kel': fields.char(
             string='Kelurahan',
+            size='100',
             ),
     }
 
