f66e7386645af2bce8751626786ea9a83fef0c00
Andhitia Rama <andhitia.r@gmail.com>
2016-02-17 10:03:57 +0700

    Modify tax checking

--- prln_invoice_vat_rounding/models/account_invoice.py
+++ prln_invoice_vat_rounding/models/account_invoice.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from osv import osv
+from tools.translate import _
 
 
 class account_invoice(osv.osv):
@@ -29,3 +30,23 @@ class account_invoice(osv.osv):
             self.pool.get('account.invoice').write(cr, uid, ids, {'invoice_line':[]}, context=ctx)
 
 
+    def check_tax_lines(self, cr, uid, inv, compute_taxes, ait_obj):
+        if not inv.tax_line:
+            for tax in compute_taxes.values():
+                ait_obj.create(cr, uid, tax)
+        else:
+            tax_key = []
+            for tax in inv.tax_line:
+                if tax.manual:
+                    continue
+                key = (tax.tax_code_id.id, tax.base_code_id.id, tax.account_id.id)
+                tax_key.append(key)
+                if not key in compute_taxes:
+                    raise osv.except_osv(_('Warning !'), _('Global taxes defined, but they are not in invoice lines !'))
+                # base = compute_taxes[key]['base']
+                base = inv.amount_untaxed
+                if abs(base - tax.base) > inv.company_id.currency_id.rounding:
+                    raise osv.except_osv(_('Warning !'), _('Tax base different!\nClick on compute to update the tax base.'))
+            for key in compute_taxes:
+                if not key in tax_key:
+                    raise osv.except_osv(_('Warning !'), _('Taxes are missing!\nClick on compute button.'))
