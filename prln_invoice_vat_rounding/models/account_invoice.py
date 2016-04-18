013e4107f11cc1458d008d1fc5a3cdcc60170508
Andhitia Rama <andhitia.r@gmail.com>
2016-04-19 03:26:43 +0700

    Hail merry #2

--- prln_invoice_vat_rounding/models/account_invoice.py
+++ prln_invoice_vat_rounding/models/account_invoice.py
@@ -24,7 +24,8 @@ class account_invoice(osv.osv):
             for line in invoice.invoice_line:
                 res[invoice.id]['amount_base'] += line.price_subtotal_base
                 res[invoice.id]['amount_discount'] += line.discount_amount_total
-                res[invoice.id]['amount_untaxed'] += line.price_subtotal
+                res[invoice.id]['amount_untaxed'] += line.price_subtotal - \
+                    res[invoice.id]['amount_untaxed'] - res[invoice.id]['amount_discount']
             for line in invoice.tax_line:
                 res[invoice.id]['amount_tax'] += line.amount
 
