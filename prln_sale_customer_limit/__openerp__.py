914f13a6f3e1ccb930b3ae0177cee724403f74ec
Andhitia Rama <andhitia.r@gmail.com>
2016-04-27 07:20:56 +0700

    [ADD] prln_sale_customer_limit

---
+++ prln_sale_customer_limit/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2015 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sale Order Customer Limit',
+    'version': '1.0.0',
+    'author': "Andhitia Rama,OpenSynergy Indonesia",
+    'license': 'AGPL-3',
+    'category': 'Sale',
+    'depends': [
+        'pralon_sale_enhancements',
+    ],
+    'description': """
+    """,
+    'website': 'http://opensynergy-indonesia.com',
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
---
+++ prln_sale_customer_limit/models/sale_order.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# © 2015 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from osv import fields, osv
+import decimal_precision as dp
+from tools.translate import _
+import netsvc
+
+
+class sale_order(osv.osv):
+    _name = 'sale.order'
+    _inherit = 'sale.order'
+
+    _columns = {
+        'credit_limit': fields.float(
+            string='Credit Limit',
+            ),
+        }
+
+    def button_check_limit(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        obj_sale = self.pool.get('sale.order')
+
+        for order in obj_sale.browse(cr, uid, ids):
+            self._check_limit(cr, uid, order)
+
+    def _check_limit(self, cr, uid, order):
+        if order.partner_id.credit_limit < order.amount_total:
+            raise osv.except_osv(_('Warning'),_('Insufficient Limit'))
+
+        wkf_service = netsvc.Localservice('workflow')
+        wkf_service.trg_validate(uid, 'sale.order', order.id, 'order_confirm')
+
+
+
